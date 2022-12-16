83b0f622894f69be75cd7a2bbeed27e11efd26a7
daiyunzhou <daiyunz@chanjet.com>
2022-12-16 14:13:10 +0800

    fix: add use ref

--- src/Component/TestUseRef/index.tsx
+++ src/Component/TestUseRef/index.tsx
@@ -10,7 +10,7 @@ import React, { useState, useRef } from "react";
 import { Button } from "antd";
 
 function TestUseRef() {
-  const inputEl:any = useRef(null);
+  const inputEl: any = useRef(null);
   const onButtonClick = () => {
     // `current` 指向已挂载到 DOM 上的文本输入元素
     inputEl.current.focus();
@@ -21,6 +21,10 @@ function TestUseRef() {
         useRef 返回一个可变的 ref 对象，其 .current
         属性被初始化为传入的参数（initialValue）。返回的 ref
         对象在组件的整个生命周期内保持不变。
+      </p>
+      <p>
+        useRef() 比 ref 属性更有用。它可以很方便地保存任何可变值，其类似于在
+        class 中使用实例字段的方式。
       </p>
       <>
         <input ref={inputEl} type="text" />
